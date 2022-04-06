10ac74e1caccfeeec059ff8f3830dd358d1dc2f1
sarveshped30 <sarveshpednekar30@gmail.com>
2022-04-07 00:22:02 +0530

    initial commit

---
+++ Elements.java
@@ -0,0 +1,10 @@
+
+public class Elements {
+	public static void main(String[] args) {
+		int array[] =  {23,45,67,32};
+		
+		for(int i = 0; i < 4; i++) {
+			System.out.println("array" + "[" + i + "] = " + array[i]);
+		}
+	}
+}
