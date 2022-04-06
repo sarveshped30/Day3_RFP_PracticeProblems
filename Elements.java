63738b0f2c1e4f43071a7b4af8490db1ea669cb7
sarveshped30 <sarveshpednekar30@gmail.com>
2022-04-07 00:32:14 +0530

    adding changes to file

--- Elements.java
+++ Elements.java
@@ -2,7 +2,10 @@
 public class Elements {
 	public static void main(String[] args) {
 		int array[] =  {23,45,67,32};
-		
+		Elements.elPrint(array);
+	}
+	
+	static void elPrint(int array[]) {
 		for(int i = 0; i < 4; i++) {
 			System.out.println("array" + "[" + i + "] = " + array[i]);
 		}
